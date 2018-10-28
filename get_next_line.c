89e02be34272d0228d58d023529f1a89b5351214
Victor Forget <viforget@w-r2-p4.s19.be>
2018-10-28 18:05:31 +0100

    Avancee du get next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: viforget <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/28 01:35:11 by viforget          #+#    #+#             */
-/*   Updated: 2018/10/28 10:13:59 by viforget         ###   ########.fr       */
+/*   Updated: 2018/10/28 17:41:24 by viforget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,21 @@ static	char	*ft_strnjoin(char const *s1, char const *s2, size_t n)
 	return (str);
 }
 
-static	size_t		ft_find_n(char *str, size_t n)
+void	ft_shift(char *s, int n)
 {
-	size_t i;
+	while (s[n])
+	{
+		s[n] = s[n + 1];
+		n++;
+	}
+}
+
+static	size_t		fnd_n(char *str, size_t n)
+{
+	size_t	i;
 
 	i = 0;
-	while(n > 0 && str[i] != '\0')
+	while (n > 0 && str[i] != '\0')
 		{
 			if (str[i] == '\n')
 				n--;
@@ -54,7 +63,7 @@ static	size_t		ft_find_n(char *str, size_t n)
 	return (i);
 }
 
-int					ft_min(int a, int b)
+static	int			ft_min(int a, int b)
 {
 	if (a < b)
 		return (a);
@@ -67,42 +76,27 @@ int					get_next_line(const int fd, char **line)
 	static	int		nb = BUFF_SIZE;
 	static	char	cstr[BUFF_SIZE + 1];
 	char			*str;
-	int				a;
 	int				check;
 	int				fndn;
-	int nperm = 0;
 
-	//ft_putstr("\nA\n");
-	//ft_putstr(cstr);
-	//ft_putstr("\nB\n");
-	//if (ft_find_n(cstr, 2) < ft_strlen(cstr))
-	//	check = 0;
-	//else
-	if (nb < BUFF_SIZE)
-		return (0);
 	check = 1;
-	fndn = ft_find_n(cstr, 1);
-
-	str = ft_strsub(cstr, fndn, ft_find_n(cstr, 2) - fndn);
-	if (ft_find_n(str, 1) == ft_strlen(str))
-		while (nb == BUFF_SIZE && check)
+	fndn = fnd_n(cstr, 1);
+	str = ft_strsub(cstr, fndn, fnd_n(cstr, 2) - fndn);
+	while (nb == BUFF_SIZE && check)
+	{
+		ft_bzero(cstr, BUFF_SIZE + 1);
+		nb = read(fd, cstr, BUFF_SIZE);
+		cstr[nb + 1] = '\0';
+		if (ft_strchr(cstr, '\n'))
 		{
-			ft_bzero(cstr, BUFF_SIZE + 1);
-			nb = read(fd, cstr, BUFF_SIZE);
-			cstr[nb + 1] = '\0';
-			if (ft_strchr(cstr, '\n'))
-			{
-				//cstr[ft_find_n(cstr) + 1] = '\0';
-				check = 0;
-			}
-			str = ft_strnjoin(str, cstr, ft_find_n(cstr, 1));
+			check = 0;
 		}
-	else
-		ft_memcpy(cstr, str, ft_min(ft_strlen(str), BUFF_SIZE));
-
-
+		str = ft_strnjoin(str, cstr, fnd_n(cstr, 1));
+	}
 	str[ft_strlen(str) - 1] = '\0';
 	*line = str;
+	if (cstr[fnd_n(cstr, 1)] == '\n')
+		ft_shift(cstr, fnd_n(cstr, 1));
 	if (nb > 0)
 		return (1);
 	else if (nb == 0)
@@ -111,15 +105,15 @@ int					get_next_line(const int fd, char **line)
 		return (-1);
 }
 
-/*int		main()
+/*
+**int		main()
 **{
 **	char *str;
 **	int a;
 **	int b;
-**
 **	b = 1;
 **	a = open("txt.txt", O_RDONLY);
-**	while (b != 0)
+**	while (b)
 **	{
 **		b = get_next_line(a, &str);
 **		printf("%s\n", str);
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: viforget <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/28 01:37:54 by viforget          #+#    #+#             */
-/*   Updated: 2018/10/28 02:42:03 by viforget         ###   ########.fr       */
+/*   Updated: 2018/10/28 17:35:09 by viforget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define BUFF_SIZE 32
+#define BUFF_SIZE 8
 
 #include <sys/types.h>
 #include <sys/stat.h>
