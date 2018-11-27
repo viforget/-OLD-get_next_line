59b9eb64d6d92da9001af05bc3f9ed3acc3f19be
Victor Forget <viforget@w-r2-p4.s19.be>
2018-11-27 15:07:02 +0100

    Memory leaks check

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,50 @@
 /*   By: viforget <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 23:01:17 by viforget          #+#    #+#             */
-/*   Updated: 2018/11/16 20:12:11 by viforget         ###   ########.fr       */
+/*   Updated: 2018/11/27 14:59:10 by viforget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+#include "libft.h"
+
+static	char	*ft_strnjoindel(char *s1, char const *s2, size_t n)
+{
+	int		i;
+	int		i2;
+	char	*str;
+	int		len;
+
+	i = 0;
+	i2 = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i2] = s1[i];
+		i++;
+		i2++;
+	}
+	i = 0;
+	while (s2[i] != '\0' && n--)
+		str[i2++] = s2[i++];
+	str[i2] = '\0';
+	ft_strdel(&s1);
+	return (str);
+}
+
 static	char	*ft_strcut(char (*str)[BUFF_SIZE], int n)
 {
 	int i;
-	
+
 	if (n == 0)
 		return (*str);
 	i = 0;
-	while((*str)[n + i])
+	while ((*str)[n + i])
 	{
 		(*str)[i] = (*str)[i + n];
 		i++;
@@ -44,16 +74,17 @@ int				get_next_line(const int fd, char **line)
 	int				rd;
 	char			*lin;
 
-	lin = ft_strnew(0);
+	if (!(lin = ft_strnew(0)) || fd < 0 || !line)
+		return (-1);
 	rd = BUFF_SIZE;
 	ft_strcut(&buf, strlenb(buf) + 1);
-	lin = ft_strnjoin(lin, buf, strlenb(buf));
-	while(strlenb(buf) == (int)ft_strlen(buf) && rd == BUFF_SIZE)
+	lin = ft_strnjoindel(lin, buf, strlenb(buf));
+	while (strlenb(buf) == (int)ft_strlen(buf) && rd == BUFF_SIZE)
 	{
 		ft_bzero(buf, BUFF_SIZE);
 		rd = read(fd, buf, BUFF_SIZE);
 		if (rd > 0)
-			lin = ft_strnjoin(lin, buf, strlenb(buf));
+			lin = ft_strnjoindel(lin, buf, strlenb(buf));
 	}
 	*line = lin;
 	if (rd < 0)
@@ -62,30 +93,3 @@ int				get_next_line(const int fd, char **line)
 		return (0);
 	return (1);
 }
-
-/*
-#include <stdio.h>
-int	main(int ac, char **argv)
-{
-	char *str;
-	int a;
-	int b;
-	int c = ft_atoi(argv[1]);
-	b = 1;
-	a = open("txt2.txt", O_RDONLY);
-	while (c--)
-	{
-		b = get_next_line(a, &str);
-		ft_putnbr(b);
-    //printf("%s\n", str);
-		free(str);
-	}
-  return (0);
-/\*	
-	char str[] = "Hey sal";
-	ft_putstr(str);
-	ft_strcut(&str, 4);
-	ft_putstr(str);
-*\/
-}*/
-
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,19 @@
 /*   By: viforget <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/28 01:37:54 by viforget          #+#    #+#             */
-/*   Updated: 2018/10/30 20:47:12 by viforget         ###   ########.fr       */
+/*   Updated: 2018/11/27 12:56:23 by viforget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define BUFF_SIZE 28
+# define BUFF_SIZE 8
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
 
